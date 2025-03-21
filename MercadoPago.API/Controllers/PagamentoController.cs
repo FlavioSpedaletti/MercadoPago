7093bec37cc3c2b0ecf611b5f3bf7982f25b78bb
Flavio Spedaletti <flavio.spedaletti@adiq.com.br>
2025-03-21 18:08:10 -0300

    pix (sem webhook ainda)

--- MercadoPago.API/Controllers/PagamentoController.cs
+++ MercadoPago.API/Controllers/PagamentoController.cs
@@ -33,5 +33,24 @@ namespace MercadoPago.Controllers
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpPost("Pix")]
+        public async Task<IActionResult> Pix([FromBody] PaymentCreateRequest paymentRequest)
+        {
+            try
+            {
+                var requestOptions = new RequestOptions();
+                requestOptions.CustomHeaders.Add("x-idempotency-key", Guid.NewGuid().ToString());
+
+                var client = new PaymentClient();
+                Payment payment = await client.CreateAsync(paymentRequest, requestOptions);
+
+                return Ok(payment.Status);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
